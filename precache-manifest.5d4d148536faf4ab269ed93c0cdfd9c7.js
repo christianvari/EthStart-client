ae669c104957f5946d1f7843c78e28b325dd2073
christianvari <christianvari@users.noreply.github.com>
2020-09-24 11:00:21 +0000

    deploy: c3d2f571e937c78765e954ffc8c4ee2e3ad664b4

---
+++ precache-manifest.5d4d148536faf4ab269ed93c0cdfd9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdfd7503e0116e893fe1d9efb83d943",
+    "url": "/EthStart-client/index.html"
+  },
+  {
+    "revision": "1d4ff1adb9f18cd63141",
+    "url": "/EthStart-client/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1d4ff1adb9f18cd63141",
+    "url": "/EthStart-client/static/js/2.dc2fc3e7.chunk.js"
+  },
+  {
+    "revision": "58c0871c16de9664e9cf9890d4f91aff",
+    "url": "/EthStart-client/static/js/2.dc2fc3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4cf35b008f2eda258ae",
+    "url": "/EthStart-client/static/js/main.94bfebc6.chunk.js"
+  },
+  {
+    "revision": "208fbcc869dbc0c0647d",
+    "url": "/EthStart-client/static/js/runtime-main.e3e89cfa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bc52ab5e4d53340a8d77baf022d635e.js"
+  "/EthStart-client/precache-manifest.5d4d148536faf4ab269ed93c0cdfd9c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EthStart-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
