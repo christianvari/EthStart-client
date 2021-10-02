e44d6b42e72daea55be6b77f32c46197876acbcb
Christian Vari <vari.christian@gmail.com>
2021-10-02 08:52:08 +0200

    implemented chain switcher

--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -7,17 +7,7 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useHistory } from "react-router-dom";
 import { shortenAddress, useEthers } from "@usedapp/core";
-import {
-    Divider,
-    Drawer,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-} from "@mui/material";
-import { Box } from "@mui/system";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import SideDrawer from "./SideDrawer";
 
 const Navigation = () => {
     const history = useHistory();
@@ -28,50 +18,7 @@ const Navigation = () => {
 
     return (
         <div className="Navigation">
-            <Drawer open={isDrawerOpen} onClose={() => setDrawerOpen(false)}>
-                <Box
-                    sx={{ width: 250 }}
-                    role="presentation"
-                    onClick={() => setDrawerOpen(false)}
-                    onKeyDown={() => setDrawerOpen(false)}
-                >
-                    <List>
-                        <ListItem
-                            button
-                            onClick={() => {
-                                history.push("/");
-                            }}
-                        >
-                            {/* <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon> */}
-                            <ListItemText primary="Home" />
-                        </ListItem>
-                        <ListItem
-                            button
-                            onClick={() => {
-                                history.push("/new");
-                            }}
-                        >
-                            {/* <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon> */}
-                            <ListItemText primary="New Campaign" />
-                        </ListItem>
-                    </List>
-                    <Divider />
-                    <List>
-                        {["All mail", "Trash", "Spam"].map((text, index) => (
-                            <ListItem button key={text}>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
-            </Drawer>
+            <SideDrawer isDrawerOpen={isDrawerOpen} setDrawerOpen={setDrawerOpen} />
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
---
+++ src/components/Layout/SideDrawer.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { ChainId, useEthers } from "@usedapp/core";
+import { Divider, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import { Box } from "@mui/system";
+
+const chainSwitch = (id, library) => {
+    library.provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [
+            {
+                chainId: `0x${id.toString(16)}`,
+            },
+        ],
+    });
+};
+
+const SideDrawer = ({ isDrawerOpen, setDrawerOpen }) => {
+    const history = useHistory();
+    const { library } = useEthers();
+
+    return (
+        <Drawer open={isDrawerOpen} onClose={() => setDrawerOpen(false)}>
+            <Box
+                sx={{ width: 250 }}
+                role="presentation"
+                onClick={() => setDrawerOpen(false)}
+                onKeyDown={() => setDrawerOpen(false)}
+            >
+                <List>
+                    <ListItem
+                        button
+                        onClick={() => {
+                            history.push("/");
+                        }}
+                    >
+                        {/* <ListItemIcon>
+                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                    </ListItemIcon> */}
+                        <ListItemText primary="Home" />
+                    </ListItem>
+                    <ListItem
+                        button
+                        onClick={() => {
+                            history.push("/new");
+                        }}
+                    >
+                        {/* <ListItemIcon>
+                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                    </ListItemIcon> */}
+                        <ListItemText primary="New Campaign" />
+                    </ListItem>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem
+                        button
+                        onClick={() => {
+                            chainSwitch(ChainId.Ropsten, library);
+                        }}
+                    >
+                        <ListItemText primary="Ethereum Ropsten" />
+                    </ListItem>
+                    <ListItem
+                        button
+                        onClick={() => {
+                            chainSwitch(
+                                parseInt(process.env.REACT_APP_NETWORKID_CRONOS),
+                                library,
+                            );
+                        }}
+                    >
+                        <ListItemText primary="Crypto.org Cronos" />
+                    </ListItem>
+                </List>
+            </Box>
+        </Drawer>
+    );
+};
+
+export default SideDrawer;
