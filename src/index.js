25bf74262e613c1308e02970987fe91ffd60c85a
Christian Vari <vari.christian@gmail.com>
2021-10-04 12:11:27 +0200

    removed basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import "./index.css";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router basename="/EthStart-client">
+        <Router>
             <DAppProvider config={DappConf}>
                 <Layout>
                     <Switch>
